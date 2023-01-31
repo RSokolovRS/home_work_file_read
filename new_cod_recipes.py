366929daf6c0ecbd310bb52d7342df8ebf2ec823
RSokolov <roman-27@inbox.ru>
2023-01-31 16:34:07 +0300

    add new cod

---
+++ new_cod_recipes.py
@@ -0,0 +1,28 @@
+cook_book = {
+  'Омлет': [
+    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+    ],
+  'Утка по-пекински': [
+    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+    ],
+  'Запеченный картофель': [
+    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+    ]
+  }
+def get_shop_list_by_dishes(dishes, person_count):
+    dish_string = ', '.join(dishes)
+    for k in cook_book:
+        if k in dish_string:
+            for i in cook_book[k]:
+                d = i.pop('ingredient_name')
+                print(d,':', i)
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
